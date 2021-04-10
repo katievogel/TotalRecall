6350e8f1d02935ef4e252feb1aa22ea152a9a83d
Katie Vogel <kvog983@gmail.com>
2021-04-10 13:53:23 -0400

    state change on tile click

--- src/com/github/katievogel/totalrecall/play_action.cljs
+++ src/com/github/katievogel/totalrecall/play_action.cljs
@@ -1,12 +1,38 @@
-(ns com.github.katievogel.totalrecall.play-action)
+(ns com.github.katievogel.totalrecall.play-action
+  (:require [com.github.katievogel.totalrecall.state :refer [initial-state]]
+            [re-frame.core :as rf]))
 
 
-;player clicks square
-;square flips
-;player clicks another square
-;square flips
-;if matches, squares stay flipped
+;player clicks tile
+;tile flips
+;player clicks another tile
+;tile flips
+;if matches, tiles stay flipped
 ;if wrong, flip back over
+(rf/reg-event-db
+  :pick-tile
+  (fn [db [_ record]]
+    (cond
+      (= (:first-pick db) nil) (assoc db :first-pick record)
+      (= (:second-pick db) nil) (assoc db :second-pick record)
+      :else nil)))
 
-(defn click-tile [first-pick second-pick]
-  (let))
+(comment
+  (= :first-pick nil))
+
+(rf/reg-sub
+  :get-db
+  (fn [db [_]]
+    db))
+;(rf/reg-sub
+;  :get-first-tile-pick
+;  (fn [db [_]]
+;    (:first-pick db)))
+;
+;(rf/reg-sub
+;  :get-second-tile-pick
+;  (fn [db [_]]
+;    (:second-pick db)))
+
+;(defn click-tile [first-pick second-pick]
+;  (let))
