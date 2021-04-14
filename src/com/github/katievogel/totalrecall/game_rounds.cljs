d81e110288efc51376b1440f872d3b5d5bf4f5e0
Katie Vogel <kvog983@gmail.com>
2021-04-14 19:07:15 -0400

    reduced files

--- src/com/github/katievogel/totalrecall/game_rounds.cljs
+++
@@ -1,7 +0,0 @@
-(ns com.github.katievogel.totalrecall.game-rounds)
-
-
-;start button displays board
-;tiles up for 3s
-;tiles flip over
-;next button generates new randomized board
--- src/com/github/katievogel/totalrecall/user_reg.cljs
+++
@@ -1,5 +0,0 @@
-(ns com.github.katievogel.totalrecall.user-reg)
-
-;player creates a user name
-;displays on the screen
-;populates in app-db
