057c758910e0518f605fefb5ace6811eb1178242
Katie Vogel <kvog983@gmail.com>
2021-04-15 20:26:55 -0400

    restrictions on next and start button

--- src/com/github/katievogel/totalrecall/views.cljs
+++ src/com/github/katievogel/totalrecall/views.cljs
@@ -1,7 +1,23 @@
 (ns com.github.katievogel.totalrecall.views
   (:require [com.github.katievogel.totalrecall.tile-boards :refer [TileBoard]]
-            [com.github.katievogel.totalrecall.scoring :refer [ScoreBoard]]
             [re-frame.core :as rf]))
+
+(defn ScoreBoard []
+  [:div.scoreboard
+   [:p.scoring "1 point per pair matched"]
+   [:div  "Score: " @(rf/subscribe [:show-score])]
+   [:div "Strikes: " @(rf/subscribe [:show-strikes])]])
+
+(defn StartGame []
+  [:button {:style    {:display (if @(rf/subscribe [:get-start-display]) "none" nil)}
+            :on-click (fn []
+                        (rf/dispatch [:hide-start])
+                        (rf/dispatch [:shuffle-tiles]))} "Start Game"])
+
+(defn NextBoard []
+  (let [strikes @(rf/subscribe [:show-strikes])]
+    [:button {:disabled (>= strikes 3)
+              :on-click (fn [] (rf/dispatch [:shuffle-tiles]))} "Next Board"]))
 
 (defn TotalRecallApp []
   [:div
@@ -9,11 +25,7 @@
    [ScoreBoard]
    [:button {:on-click (fn [] (rf/dispatch [:temp-clear]))}
     "temp - clear picks"]
+   [NextBoard]
+   [StartGame]
    [TileBoard]
    [:pre (pr-str @(rf/subscribe [:get-db]))]])
-
-
-;headers
-;start button / grid
-;scoreboard
-;next button
