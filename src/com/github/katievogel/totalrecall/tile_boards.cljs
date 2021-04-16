79481a661460a639e813c61c1f855ba6e879098a
Katie Vogel <kvog983@gmail.com>
2021-04-15 20:26:28 -0400

    cleared randomizer from Tileboard, moved map

--- src/com/github/katievogel/totalrecall/tile_boards.cljs
+++ src/com/github/katievogel/totalrecall/tile_boards.cljs
@@ -3,43 +3,19 @@
             [reagent.dom]
             [re-frame.core :as rf]))
 
-;grid of tiles with images
-;grid must be randomizable to create new game
-
-(def tile-pair-map [{:id 1 :pair 1 :face-up false}
-                    {:id 2 :pair 1 :face-up false}
-                    {:id 3 :pair 2 :face-up false}
-                    {:id 4 :pair 2 :face-up false}
-                    {:id 5 :pair 3 :face-up false}
-                    {:id 6 :pair 3 :face-up false}
-                    {:id 7 :pair 4 :face-up false}
-                    {:id 8 :pair 4 :face-up false}
-                    {:id 9 :pair 5 :face-up false}
-                    {:id 10 :pair 5 :face-up false}
-                    {:id 11 :pair 6 :face-up false}
-                    {:id 12 :pair 6 :face-up false}
-                    {:id 13 :pair 7 :face-up false}
-                    {:id 14 :pair 7 :face-up false}
-                    {:id 15 :pair 8 :face-up false}
-                    {:id 16 :pair 8 :face-up false}
-                    {:id 17 :pair 9 :face-up false}
-                    {:id 18 :pair 9 :face-up false}
-                    {:id 19 :pair 10 :face-up false}
-                    {:id 20 :pair 10 :face-up false}])
-
-(defn tile-randomizer [tile-pair-map]
-  (for [{:keys [pair id] :as record}
-        (shuffle tile-pair-map)]
-    ^{:key id} [:div.col [:div.p-3.border.bg-light
-                          {:on-click (fn []
-                                       (rf/dispatch [:pick-tile record])
-                                       (println "clicked, id: " id))}
-                          "pair:" pair " id:" id]]))
+(defn render-tile []
+  (let [board @(rf/subscribe [:get-board])]
+    (for [{:keys [pair id] :as record}
+          board]
+      ^{:key id} [:div.col [:div.p-3.border.bg-light
+                            {:on-click (fn []
+                                         (rf/dispatch [:pick-tile record])
+                                         (println "clicked, id: " id))}
+                            "pair:" pair " id:" id]])))
 
 (defn TileBoard []
   [:div.container
    [:div.row.row-cols-4.row-cols-lg-5.g2.g-lg-3
-    (tile-randomizer tile-pair-map)]])
-
+    (render-tile)]])
 
 
