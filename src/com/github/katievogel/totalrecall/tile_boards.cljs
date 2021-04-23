4c6b43a311c8d944e44c79e5a48822465a1c3461
Katie Vogel <kvog983@gmail.com>
2021-04-22 21:13:12 -0400

    disabling click for pairs picked

--- src/com/github/katievogel/totalrecall/tile_boards.cljs
+++ src/com/github/katievogel/totalrecall/tile_boards.cljs
@@ -3,15 +3,23 @@
             [reagent.dom]
             [re-frame.core :as rf]))
 
+
 (defn render-tile []
-  (let [board @(rf/subscribe [:get-board])]
-    (for [{:keys [pair id image] :as record}
+  (let [board @(rf/subscribe [:get-board])
+        check-pairs @(rf/subscribe [:check-cleared-pairs])]
+    (for [{:keys [_pair id image] :as record}
           board]
-      ^{:key id} [:div.col [:div.p-3.border.bg-light
-                            {:on-click (fn []
-                                         (rf/dispatch [:pick-tile record])
-                                         (println "clicked, id: " id))}
-                            [:img {:src image}]]])))
+      ^{:key id}
+      [:div.col
+       [:div.p-3.border.bg-light
+         {:class [(when (contains? check-pairs id) "disabled")]
+          :on-click (when-not (contains? check-pairs id)
+                        (fn []
+                          (rf/dispatch [:pick-tile record])
+                          (rf/dispatch [:pair-complete])
+                          (println "clicked, id: " id)))}
+        [:img {:src image}]]])))
+
 
 (defn TileBoard []
   [:div.container
