fa9aa58d6070ded75911972bc6eae5bfc49025f1
Katie Vogel <kvog983@gmail.com>
2021-04-24 16:19:33 -0400

    title and score bar

--- src/com/github/katievogel/totalrecall/tile_boards.cljs
+++ src/com/github/katievogel/totalrecall/tile_boards.cljs
@@ -7,18 +7,21 @@
 (defn render-tile []
   (let [board @(rf/subscribe [:get-board])
         check-pairs @(rf/subscribe [:check-cleared-pairs])]
-    (for [{:keys [_pair id image] :as record}
+    (for [{:keys [_pair id image face-up] :as record}
           board]
       ^{:key id}
       [:div.col
-       [:div.p-3.border.bg-light
-         {:class [(when (contains? check-pairs id) "disabled")]
-          :on-click (when-not (contains? check-pairs id)
-                        (fn []
-                          (rf/dispatch [:pick-tile record])
-                          (rf/dispatch [:pair-complete])
-                          (println "clicked, id: " id)))}
-        [:img {:src image}]]])))
+       [:div.p-3.border.bg-light.flip-card
+        [:div.flip-card-inner
+         [:div.flip-card-front
+          {:class    [(when (contains? check-pairs id) "disabled")]
+           :on-click (when-not (contains? check-pairs id)
+                       (fn []
+                         (rf/dispatch [:pick-tile record])
+                         (rf/dispatch [:pair-complete])
+                         (println "clicked, id: " id)))}
+          [:img {:src (if face-up image)}]]
+         [:div.flip-card-back]]]])))
 
 
 (defn TileBoard []
