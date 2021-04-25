f6861bd202b8f6bc5c6ed73531f92645a7d0f16a
Katie Vogel <kvog983@gmail.com>
2021-04-24 20:30:50 -0400

    doall to fix lazy seq bug, get tiles by id

--- src/com/github/katievogel/totalrecall/tile_boards.cljs
+++ src/com/github/katievogel/totalrecall/tile_boards.cljs
@@ -7,21 +7,22 @@
 (defn render-tile []
   (let [board @(rf/subscribe [:get-board])
         check-pairs @(rf/subscribe [:check-cleared-pairs])]
-    (for [{:keys [_pair id image face-up] :as record}
-          board]
-      ^{:key id}
-      [:div.col
-       [:div.p-3.border.bg-light.flip-card
-        [:div.flip-card-inner
-         [:div.flip-card-front
-          {:class    [(when (contains? check-pairs id) "disabled")]
-           :on-click (when-not (contains? check-pairs id)
-                       (fn []
-                         (rf/dispatch [:pick-tile record])
-                         (rf/dispatch [:pair-complete])
-                         (println "clicked, id: " id)))}
-          [:img {:src (if face-up image)}]]
-         [:div.flip-card-back]]]])))
+    (doall (for [#_{:keys [_pair id image face-up] :as record}
+                 id board]
+             ^{:key id}
+             [:div.col
+              [:div.p-3.border.bg-light.flip-card
+               [:div.flip-card-inner
+                [:div.flip-card-front
+                 {:class    [(when (contains? check-pairs id) "disabled")]
+                  :on-click (when-not (contains? check-pairs id)
+                              (fn []
+                                (rf/dispatch [:pick-tile id])
+                                (rf/dispatch [:pair-complete])
+                                (println "clicked, id: " id)))}
+                 [:img {:src (let [{:keys [image face-up]} @(rf/subscribe [:get-tile id])]
+                               (if face-up image))}]]
+                [:div.flip-card-back]]]]))))
 
 
 (defn TileBoard []
