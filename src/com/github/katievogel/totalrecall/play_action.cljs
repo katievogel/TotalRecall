b9996fcd5e3ced30a8a0f115b34bb1dac8c36be3
Katie Vogel <kvog983@gmail.com>
2021-04-24 20:29:45 -0400

    refactoring to get tiles by id, keep face-up

--- src/com/github/katievogel/totalrecall/play_action.cljs
+++ src/com/github/katievogel/totalrecall/play_action.cljs
@@ -18,47 +18,62 @@
 ;if the tiles do not match, then the tiles will flip back over after 3 s,
 ;..and state updates again.
 
-(defn pick-tile-eval [db]
-  (let [first-pick-pair (get-in db [:first-pick :pair])
-        second-pick-pair (get-in db [:second-pick :pair])]
+(defn pick-tile-eval [{:keys [first-pick second-pick] :as db}]
+  (let [first-pick-pair (get-in db [:tile-pair-map first-pick :pair])
+        second-pick-pair (get-in db [:tile-pair-map second-pick :pair])]
     (cond
       (= first-pick-pair second-pick-pair) (update-in db [:score] inc)
       :else (update-in db [:strikes] inc))))
 
 (rf/reg-event-db
   :pick-tile
-  (fn [db [_ record]]
+  (fn [db [_ id]]
     (let [strikes (:strikes db)]
       (cond
         (>= strikes 3) db
-        (= (:first-pick db) nil) (assoc db :first-pick record)
+        (= (:first-pick db) nil) (-> db
+                                     (assoc :first-pick id)
+                                     (assoc-in [:tile-pair-map id :face-up] true))
         (= (:second-pick db) nil) (-> db
-                                      (assoc :second-pick record)
+                                      (assoc :second-pick id)
+                                      (assoc-in [:tile-pair-map id :face-up] true)
                                       (pick-tile-eval))
         :else db))))
 
 (rf/reg-event-db
   :shuffle-tiles
   (fn [db]
-    (assoc db :board (shuffle state/tile-pair-map))))
+    (assoc db :board (shuffle (keys (:tile-pair-map db))))))
 
 (rf/reg-event-db
   :pair-complete
-  (fn [db]
-    (let [first-pick-pair (get-in db [:first-pick :pair])
-          second-pick-pair (get-in db [:second-pick :pair])
-          first-pick-id (get-in db [:first-pick :id])
-          second-pick-id (get-in db [:second-pick :id])]
+  (fn [{:keys [first-pick second-pick] :as db}]
+    (let [first-pick-pair (get-in db [:tile-pair-map first-pick :pair])
+          second-pick-pair (get-in db [:tile-pair-map second-pick :pair])]
       (cond
-          (= first-pick-pair second-pick-pair) (update db :cleared-pairs (fn [x] (conj x first-pick-id second-pick-id)))
+          (= first-pick-pair second-pick-pair) (update db :cleared-pairs (fn [x] (conj x first-pick second-pick)))
           :else db))))
 
-(rf/reg-event-db
-  :temp-clear
-  (fn [db]
-    (-> db
-        (assoc :first-pick nil)
-        (assoc :second-pick nil))))
+;(rf/reg-event-db
+;  :temp-clear
+;  (fn [{:keys [first-pick second-pick cleared-pairs] :as db}]
+;    (let [first-desired-state (contains? cleared-pairs first-pick)
+;          second-desired-state (contains? cleared-pairs second-pick)]
+;      (-> db
+;          (assoc-in [:tile-pair-map first-pick :face-up] first-desired-state)
+;          (assoc-in [:tile-pair-map second-pick :face-up] second-desired-state)))
+;
+;
+;
+;    (if (contains? cleared-pairs first-pick)
+;      (assoc-in db [:tile-pair-map first-pick :face-up] false))
+;
+;
+;    (-> db
+;        (assoc :first-pick nil)
+;        (assoc :second-pick nil)
+;        (cond
+;          (get-in db [:cleared-pairs id])))))
 
 (rf/reg-event-db
   :hide-start
@@ -84,6 +99,12 @@
   :get-board
   (fn [db [_]]
     (:board db)))
+
+(rf/reg-sub
+  :get-tile
+  (fn [db [_ id]]
+    (get-in db [:tile-pair-map id])))
+
 
 (rf/reg-sub
   :check-cleared-pairs
