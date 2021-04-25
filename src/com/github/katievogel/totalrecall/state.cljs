fa14fdf2bfb73c67c6ad2ef725cf30872d8c7641
Katie Vogel <kvog983@gmail.com>
2021-04-24 20:29:58 -0400

    state updates to use ID

--- src/com/github/katievogel/totalrecall/state.cljs
+++ src/com/github/katievogel/totalrecall/state.cljs
@@ -1,39 +1,30 @@
-(ns com.github.katievogel.totalrecall.state
-  (:require [reagent.core :as r]))
-
-
-;app-db contains:
-;player name
-;scores
-;tiles picks, states - up or down
-;strikes
-
-(def tile-pair-map [{:id 1 :pair 1 :image "assets/cake.png" :face-up true}
-                    {:id 2 :pair 1 :image "assets/cake.png" :face-up true}
-                    {:id 3 :pair 2 :image "assets/croissant.png" :face-up true}
-                    {:id 4 :pair 2 :image "assets/croissant.png" :face-up true}
-                    {:id 5 :pair 3 :image "assets/fries.png" :face-up true}
-                    {:id 6 :pair 3 :image "assets/fries.png" :face-up true}
-                    {:id 7 :pair 4 :image "assets/hamburger.png" :face-up true}
-                    {:id 8 :pair 4 :image "assets/hamburger.png" :face-up true}
-                    {:id 9 :pair 5 :image "assets/ice-cream.png" :face-up true}
-                    {:id 10 :pair 5 :image "assets/ice-cream.png" :face-up true}
-                    {:id 11 :pair 6 :image "assets/pie.png" :face-up true}
-                    {:id 12 :pair 6 :image "assets/pie.png" :face-up true}
-                    {:id 13 :pair 7 :image "assets/pho-soup.png" :face-up true}
-                    {:id 14 :pair 7 :image "assets/pho-soup.png" :face-up true}
-                    {:id 15 :pair 8 :image "assets/pizza.png" :face-up true}
-                    {:id 16 :pair 8 :image "assets/pizza.png" :face-up true}
-                    {:id 17 :pair 9 :image "assets/sushi.png" :face-up true}
-                    {:id 18 :pair 9 :image "assets/sushi.png" :face-up true}
-                    {:id 19 :pair 10 :image "assets/taco.png" :face-up true}
-                    {:id 20 :pair 10 :image "assets/taco.png" :face-up true}])
+(ns com.github.katievogel.totalrecall.state)
 
 (def initial-state
   {:score 0
    :strikes 0
    :first-pick nil
    :second-pick nil
+   :tile-pair-map  {1  {:pair 1 :image "assets/cake.png" :face-up false}
+                    2  {:pair 1 :image "assets/cake.png" :face-up false}
+                    3  {:pair 2 :image "assets/croissant.png" :face-up false}
+                    4  {:pair 2 :image "assets/croissant.png" :face-up false}
+                    5  {:pair 3 :image "assets/fries.png" :face-up false}
+                    6  {:pair 3 :image "assets/fries.png" :face-up false}
+                    7  {:pair 4 :image "assets/hamburger.png" :face-up false}
+                    8  {:pair 4 :image "assets/hamburger.png" :face-up false}
+                    9  {:pair 5 :image "assets/ice-cream.png" :face-up false}
+                    10 {:pair 5 :image "assets/ice-cream.png" :face-up false}
+                    11 {:pair 6 :image "assets/pie.png" :face-up false}
+                    12 {:pair 6 :image "assets/pie.png" :face-up false}
+                    13 {:pair 7 :image "assets/pho-soup.png" :face-up false}
+                    14 {:pair 7 :image "assets/pho-soup.png" :face-up false}
+                    15 {:pair 8 :image "assets/pizza.png" :face-up false}
+                    16 {:pair 8 :image "assets/pizza.png" :face-up false}
+                    17 {:pair 9 :image "assets/sushi.png" :face-up false}
+                    18 {:pair 9 :image "assets/sushi.png" :face-up false}
+                    19 {:pair 10 :image "assets/taco.png" :face-up false}
+                    20 {:pair 10 :image "assets/taco.png" :face-up false}}
    :board nil
    :start-button-display false
    :cleared-pairs #{}})
